07abfb141e2e72e2b75be9f41c960eeab534ed77
Martin Safar <1martinsafar@gmail.com>
2017-10-09 12:53:51 +0200

    MediaElement.js and main layout added

---
+++ scripts.js
@@ -0,0 +1,11 @@
+$('video').mediaelementplayer({
+
+  features: ['playpause', 'tracks','progress']
+
+  // features: ['playpause', 'tracks','progress'],
+  // startLanguage: 'en'
+
+  // features: ['playpause', 'current', 'progress', 'duration', 'tracks', 'volume', 'fullscreen']
+  // playpause, current, progress, duration, tracks, volume, fullscreen
+
+});
